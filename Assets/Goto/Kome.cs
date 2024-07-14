81e2c98a788cd665cf2d6e83489c58b131e6f785
GotoYuya0412 <bantan.g300417@gmail.com>
2024-07-14 15:51:26 +0900

    moveを変更

---
+++ Assets/Goto/Kome.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kome : MonoBehaviour
+{
+    GameObject _player;
+    GameObject _laser;
+    bool _hitLaser = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameObject.Find("Suikomiguti");
+        _laser = GameObject.Find("Laser");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_hitLaser)
+        {
+
+            Vector3 suikomi = Vector3.Lerp(transform.position, _player.transform.position, Time.deltaTime * 3f);
+            transform.position = suikomi;
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Laser")
+        {
+            _hitLaser = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
