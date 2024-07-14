4274e3338b68bb51d756c7c4d85629b7c1eb2305
GC_Lapis <sinsuilapisgm@gmail.com>
2024-07-14 15:35:51 +0900

    AI逃げるの完成

--- Assets/MIura/AI.cs
+++ Assets/MIura/AI.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AI : MonoBehaviour
 {
     #region@•Ï”éŒ¾
+    #region Search()•Ï”
     [SerializeField] private Transform _myselfTransform;
     [SerializeField] private Transform _targetTransform;
     [SerializeField] private float _angle;
@@ -12,23 +14,58 @@ public class AI : MonoBehaviour
     private Ray _ray;
     private Vector3 _targetDir;
     #endregion
+    #region AiNavMehs()•Ï”
+    [SerializeField]private float _runSpeed=default;
+    [SerializeField] private float _defaultSpeed;
+    private NavMeshAgent _navMeshAgent;
+    private float _distance;
+    private bool _flag = true;
+    #endregion
+    #endregion
     #region À‘•
     private void Start()
     {
-
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        StartCoroutine(AiNavMesh());
     }
     #endregion
     #region ŠÖ”éŒ¾
 
+    private IEnumerator AiNavMesh()
+    {
+        while (true)
+        {
+            _distance = Vector3.Distance(_myselfTransform.position, _navMeshAgent.destination);
+            if (Search() && _flag)
+            {
+                _navMeshAgent.speed = _runSpeed;
+                _flag = false;
+                _navMeshAgent.destination = (_myselfTransform.position - _targetDir)*2;
+            }
+            else if (_distance <= 2.0f)
+            {
+                _navMeshAgent.speed = _defaultSpeed;
+                _navMeshAgent.destination = _myselfTransform.localPosition + RandomVector();
+                _flag = true;
+            }
+            yield return null;
+        }
+    }
+
+    private Vector3 RandomVector()
+    {
+        return new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 5));
+    }
+
     public bool Search()
     {
         var myself = _myselfTransform.position;
-        var target = _targetTransform.position;
-        var selfDir = _myselfTransform.forward;
-        _targetDir = target - myself;
-        var targetDistance = target.magnitude;
+        var targetPos = _targetTransform.position;
+        var selfDirPos = _myselfTransform.forward;
+        _targetDir = targetPos - myself;
+        var targetDistance = targetPos.magnitude;
         var cosHalf = Mathf.Cos(_angle / 2 * Mathf.Deg2Rad);
-        var innerProduct = Vector3.Dot(selfDir, _targetDir.normalized);
+        var innerProduct = Vector3.Dot(selfDirPos, _targetDir.normalized);
         if (innerProduct > cosHalf && targetDistance < _maxDistance)
         {
             _ray = new Ray(_myselfTransform.position, _targetDir);
@@ -39,7 +76,6 @@ public class AI : MonoBehaviour
                 {
                     return true;
                 }
-
             }
         }
         return false;
