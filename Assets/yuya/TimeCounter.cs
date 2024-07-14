3e3210277ad9d354801e5a9d0cecc1292757cd09
Yu_kitamura <yuu8212004@gmail.com>
2024-07-14 11:49:19 +0900

    自分用のSceneとスクリプトの追加

---
+++ Assets/yuya/TimeCounter.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeCounter : MonoBehaviour
+{
+    //カウントダウン
+    public float countdown = 5.0f;
+
+    //時間を表示するText型の変数
+    public Text timeText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //時間をカウントダウンする
+        countdown -= Time.deltaTime;
+
+        //時間を表示する
+        timeText.text = countdown.ToString("f1") + "秒";
+
+        //countdownが0以下になったとき
+        if (countdown <= 0)
+        {
+            timeText.text = "終了！";
+        }
+    }
+}
