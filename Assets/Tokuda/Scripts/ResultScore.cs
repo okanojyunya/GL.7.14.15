e36b3b00326fcc794474783619287d417bbd270e
TokudaAkito <ukameiinfu2024128@gmail.com>
2024-07-15 11:41:42 +0900

    リザルトシーンのテキスト表示修正

--- Assets/Tokuda/Scripts/ResultScore.cs
+++ Assets/Tokuda/Scripts/ResultScore.cs
@@ -5,17 +5,35 @@ using UnityEngine.UI;
 
 public class ResultScore : MonoBehaviour
 {
+    //âΩê∑ÇËÇ©
     [SerializeField] private Text _resultText;
+    //âΩó±èWÇﬂÇΩÇ©
+    [SerializeField] private Text _scoreText;
     // Start is called before the first frame update
     void Start()
     {
+        _resultText = GetComponentInChildren<Text>();
         Debug.Log(ScoreManager.score);
-        _resultText.text = ScoreManager.score.ToString();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        _scoreText.text = ScoreManager.score.ToString();
+        if (ScoreManager.score == 0)
+        {
+            _resultText.text = "Ç¥ÇÒÇÀÇÒ";
+        }
+        else if (ScoreManager.score > 0 && ScoreManager.score <= 149)
+        {
+            _resultText.text = "è¨ê∑";
+        }
+        else if (ScoreManager.score > 149 && ScoreManager.score <= 199)
+        {
+            _resultText.text = "íÜê∑";
+        }
+        else if (ScoreManager.score > 199 && ScoreManager.score <= 299)
+        {
+            _resultText.text = "ëÂê∑";
+        }
+        else if (ScoreManager.score > 299)
+        {
+            _resultText.text = "îMê∑";
+        }
     }
 }
